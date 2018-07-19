15805921899db0be2673b6e4abd44b6eabc38e66
1405043-kd <1405043-kd@ugrad.cse.buet.ac.bd>
2018-07-19 20:46:04 +0600

    #word+tag

--- gogabangla/definitions/views.py
+++ gogabangla/definitions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import logout
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from .models import Definition,Word,Tag
+from django.shortcuts import render, get_object_or_404
+from .models import Definition, Word, Tag
 
 # Create your views here.
 from django.contrib.auth.decorators import login_required
@@ -11,12 +11,42 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-def showword(request, word):
-    w=Word.objects.get(word_name=word)
-    d=Definition.objects.filter(word=w)
+
+def show_word(request, word):
+    word = Word.objects.get(word_name=word)
+    definition = Definition.objects.filter(word=word)
+
     context = {
-        'word': w,
-        'define': d,
+        'word': word,
+        'definitions': definition,
     }
     ##way 2###return HttpResponse(template.render(context, request))
-    return render(request, 'showword.html', context)
+    return render(request, 'show_word.html', context)
+
+
+def show_id_word(request, num):
+    #word = Word.objects.filter(id=num)
+    word = get_object_or_404(Word, pk=num)
+    #print(word.word_name)
+    definition = Definition.objects.filter(word=word)
+
+    context = {
+        'word': word,
+        'definitions': definition,
+    }
+    ##way 2###return HttpResponse(template.render(context, request))
+    return render(request, 'show_word.html', context)
+
+
+def show_tag(request, tag_name):
+    tag = get_object_or_404(Tag, tag=tag_name)
+    #word = get_object_or_404(Word, pk=nu)
+    #print(word.word_name)
+    definition = Definition.objects.filter(tags__tag=tag_name).distinct()
+
+    context = {
+        'tag': tag,
+        'definitions': definition,
+    }
+    ##way 2###return HttpResponse(template.render(context, request))
+    return render(request, 'show_tag.html', context)
